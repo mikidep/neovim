0f9661ac858dfb207d48e2d4618511b6c94d06f7
Michele De Pascalis <michele.de.pascalis.1024@gmail.com>
2025-01-27 21:04:11 +0200

    find hidden files

--- config/telescope.nix
+++ config/telescope.nix
@@ -10,6 +10,7 @@
       "jk".__raw = "require('telescope.actions').close";
       "kj".__raw = "require('telescope.actions').close";
     };
+    settings.pickers.find_files.hidden = true;
     settings.pickers.live_grep.glob_pattern = ["!flake.lock"];
   };
 
